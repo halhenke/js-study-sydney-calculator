a0c275633234a14992cfd873c4a40d5df3a6cd15
Hal Henke <halhenke@gmail.com>
2015-10-07 17:37:55 +1100

    Real Limited Calculator

--- src/app/calcFunctionality.js
+++ src/app/calcFunctionality.js
@@ -28,3 +28,41 @@ export const add = (n1, n2) => parseInt(n1, 10) + parseInt(n2, 10);
 export const multiply = (n1, n2) => parseInt(n1, 10) * parseInt(n2, 10);
 export const divide = (n1, n2) => parseInt(n1, 10) / parseInt(n2, 10);
 export const subtract = (n1, n2) => parseInt(n1, 10) - parseInt(n2, 10);
+
+
+export let pressed = [];
+export const queueStuff = (press) => {
+  pressed.push(press);
+};
+const empty = (result) => {
+  console.log(`empty result is ${result}`);
+  pressed = [];
+  return result;
+};
+
+export const evaluate = () => {
+    if (pressed.length < 3) {
+      return `ERROR - not enough buttons pressed`;
+    }
+    switch (true) {
+    // switch (pressed[1]) {
+      case /\+/.test(pressed[1]):
+      // case '+':
+        return empty(add(pressed[0], pressed[2]));
+        break;
+      case /-/.test(pressed[1]):
+      // case '-':
+        return empty(subtract(pressed[0], pressed[2]));
+        break;
+      case /\*/.test(pressed[1]):
+      // case '*':
+        return empty(multiply(pressed[0], pressed[2]));
+        break;
+      case /\//.test(pressed[1]):
+      // case '/':
+        return empty(divide(pressed[0], pressed[2]));
+        break;
+      default:
+        return empty(`Input needs to be 'NUM OP NUM' not ${pressed[0]} ${pressed[1]} ${pressed[2]}`);
+    }
+};
